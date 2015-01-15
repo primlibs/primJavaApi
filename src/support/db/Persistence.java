c99a13502e0eb959976e38820adcbb359ff96c89
Кот <@192.168.8.2>
2015-01-15 13:50:01 +0600

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml

--- src/support/db/Persistence.java
+++ src/support/db/Persistence.java
@@ -23,8 +23,6 @@ import support.logic.RightStack;
  */
 public class Persistence {
 
-
-
     private Persistence() {
 
     }
@@ -34,71 +32,71 @@ public class Persistence {
     }
 
     public List<Table> createTableFromJar(Class startClass) throws Exception {
-        List<Table> result= new ArrayList();
+        List<Table> result = new ArrayList();
         Collection string = JarScan.scanClasses(JarScan.getFilePathToClasses(startClass));
 
         for (Object cls : string) {
             Class cl = Class.forName(cls.toString());
-            
+
             if (cl.isAnnotationPresent(support.commons.db.Table.class)) {
-                Object obj = cl.newInstance(); 
+                Object obj = cl.newInstance();
                 result.add(createTableFromAnnotationObject(obj));
             }
         }
         return result;
     }
-    
-    public Table createTableFromAnnotationObject(Object ob) throws Exception{
-        Table result=null;
-        if(ob.getClass().isAnnotationPresent(support.commons.db.Table.class)){
+
+    public Table createTableFromAnnotationObject(Object ob) throws Exception {
+        Table result = null;
+        if (ob.getClass().isAnnotationPresent(support.commons.db.Table.class)) {
             support.commons.db.Table tabl = (support.commons.db.Table) ob.getClass().getAnnotation(support.commons.db.Table.class);
-                result=Table.getInstance(tabl.name());
-                Field[] fds = ob.getClass().getDeclaredFields();
-                for (Field fd : fds) {
-                    if (fd.isAnnotationPresent(support.commons.db.Column.class)) {
-                        Column column;
-                        support.commons.db.Column col = fd.getAnnotation(support.commons.db.Column.class);
-                        if(fd.isAnnotationPresent(support.commons.db.Primary.class)){
-                            column=Column.getInstance(col.name(), col.type(), col.isNull(), true);
-                        }else{
-                            column=Column.getInstance(col.name(), col.type(), col.isNull(), false);
-                        }
-                        result.addColumn(column);
+            result = Table.getInstance(tabl.name());
+            Field[] fds = ob.getClass().getDeclaredFields();
+            for (Field fd : fds) {
+                if (fd.isAnnotationPresent(support.commons.db.Column.class)) {
+                    Column column;
+                    support.commons.db.Column col = fd.getAnnotation(support.commons.db.Column.class);
+                    if (fd.isAnnotationPresent(support.commons.db.Primary.class)) {
+                        column = Column.getInstance(col.name(), col.type(), col.isNull(), true);
+                    } else {
+                        column = Column.getInstance(col.name(), col.type(), col.isNull(), false);
                     }
+                    result.addColumn(column);
                 }
-        }else{
-            throw new Exception("Only @table annotation resolve object "+ob.getClass());
+            }
+        } else {
+            throw new Exception("Only @table annotation resolve object " + ob.getClass());
         }
         return result;
     }
-       
+
     public RightStack createRightsFromJar(Class startclass) throws Exception {
-        RightStack result= RightStack.getInstance();
+        RightStack result = RightStack.getInstance();
         Collection string = JarScan.scanClasses(JarScan.getFilePathToClasses(startclass));
 
         for (Object cls : string) {
             Class cl = Class.forName(cls.toString());
-            
+
             if (cl.isAnnotationPresent(support.commons.Controller.class)) {
-                
-                Object obj = cl.newInstance(); 
+
+                Object obj = cl.newInstance();
                 support.commons.Controller contAnn = obj.getClass().getAnnotation(support.commons.Controller.class);
-                Method[] methods=obj.getClass().getDeclaredMethods();
-                for(Method method:methods){
-                    if(method.isAnnotationPresent(support.commons.Right.class)){
+                if(contAnn==null){
+                    throw new Exception("xxxxx");
+                }
+                Method[] methods = obj.getClass().getDeclaredMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(support.commons.Right.class)) {
                         support.commons.Right methAnn = method.getClass().getAnnotation(support.commons.Right.class);
-                        result.add(cl.getName(), method.getName(),contAnn.description(),methAnn.description());
+                        result.add(
+                                cl.getName(),
+                                method.getName());
                     }
                 }
-                
+
             }
         }
         return result;
     }
-    
-    
-    
-    
-    
-    
+
 }

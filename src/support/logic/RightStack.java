a995e2899399e13837bf5c9c122608d4e9140a3c
Кот <@192.168.0.124>
2015-04-10 15:36:03 +0500

    Merge origin/master
    Conflicts:
    	src/java/data/services/CarCompletionGroupService.java
    	src/java/data/services/CarCompletionOptionService.java
    	src/java/data/services/CarPropertyService.java
    	src/java/data/services/ColorGroupService.java
    	src/java/data/services/FeatureService.java

--- src/support/logic/RightStack.java
+++ src/support/logic/RightStack.java
@@ -24,12 +24,10 @@ public class RightStack {
         return new RightStack();
     }
     
-    public void add(String object,String action) throws Exception{
-            rights.add(Right.valueOf(object, action));
-    }
-    
     public void add(String object,String action,String objectDescription,String actionDescription) throws Exception{
-            rights.add(Right.valueOf(object, action,objectDescription,actionDescription));
+            if(!isRight(object, action)){
+                rights.add(Right.valueOf(object, action,objectDescription,actionDescription));
+            }
     }
     
     public boolean isRight(String object,String action){
